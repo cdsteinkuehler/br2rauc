32ac02e448e7fb7ee8968d222fbfc5ef88c9d228
Charles Steinkuehler <charles@steinkuehler.net>
2022-02-27 13:27:22 -0600

    Add support for rescue partition
    Add squashfs rescue partition to use in recovery mode
    Add support for entering recovery mode in u-boot script (tie GPIO4 low)
    Modify vfat partition size and location to be generally consistent with
    RPi OS images (256M starting at 4M)
    Add upload partition and increase size of rootfs partitions while
    still targeting a 4GB uSD
    Increase U-Boot environment to 32K and relocate
    
    Signed-off-by: Charles Steinkuehler <charles@steinkuehler.net>

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -10,10 +10,16 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
-# Mount persistent data partition
+# Mount persistent data partitions
 if [ -e ${TARGET_DIR}/etc/fstab ]; then
+	# For configuration data
+	# WARNING: data=journal is safest, but potentially slow!
 	grep -qE 'LABEL=Data' ${TARGET_DIR}/etc/fstab || \
 	echo "LABEL=Data /data ext4 defaults,data=journal,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
+
+	# For bulk data (eg: firmware updates)
+	grep -qE 'LABEL=Upload' ${TARGET_DIR}/etc/fstab || \
+	echo "LABEL=Upload /upload ext4 defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
 fi
 
 # Copy custom cmdline.txt file
