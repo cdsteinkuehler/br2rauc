ff5a8601d20a448a9217f5d67aadd7c0d376a643
Leonardo Amorim <leonardoaraujodf@gmail.com>
2024-07-26 18:28:05 -0300

    Add support for Raspberry Pi 5
    - Add the various configurations files
    to give support for the Pi5 in the br2rauc
    project.
    - The raspberrypi5-rauc_defconfig is using (for now)
    the buildroot provided device-tree.
    
    Signed-off-by: Leonardo Amorim <leonardoaraujodf@gmail.com>

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -4,7 +4,8 @@ set -u
 set -e
 
 RAUC_COMPATIBLE="${2:-br2rauc-rpi4-64}"
-
+BOARD_DIR="$(dirname $0)"
+BOARD_NAME="$(basename ${BOARD_DIR})"
 # Pass VERSION as an environment variable (eg: export from a top-level Makefile)
 # If VERSION is unset, fallback to the Buildroot version
 RAUC_VERSION=${VERSION:-${BR2_VERSION_FULL}}
@@ -35,7 +36,11 @@ if [ -e ${TARGET_DIR}/etc/fstab ]; then
 fi
 
 # Copy custom cmdline.txt file
-install -D -m 0644 ${BR2_EXTERNAL_BR2RAUC_PATH}/board/raspberrypi/cmdline.txt ${BINARIES_DIR}/custom/cmdline.txt
+if [ ${BOARD_NAME} = "raspberrypi5" ]; then
+    install -D -m 0644 ${BR2_EXTERNAL_BR2RAUC_PATH}/board/raspberrypi/cmdline_5.txt ${BINARIES_DIR}/custom/cmdline.txt
+else
+    install -D -m 0644 ${BR2_EXTERNAL_BR2RAUC_PATH}/board/raspberrypi/cmdline.txt ${BINARIES_DIR}/custom/cmdline.txt
+fi
 
 # Copy RAUC certificate
 if [ -e ${BR2_EXTERNAL_BR2RAUC_PATH}/openssl-ca/dev/ca.cert.pem ]; then
